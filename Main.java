85512469a91d1c744ae5121f5dfe4e2e2b5011fb
Keegan Jamison Welch <keeganjamisonwelch@gmail.com>
2025-02-03 22:17:45 -0500

    created "addObject" method

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 import java.io.*;
+import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws Exception {
+        addObject(new Item("TEST", "OBJECT"), new File("test.txt"));
+
         for (Item i : getObjects(new File("test.txt"))) {
             System.out.println(i.getKey() + ", " + i.getValue());
         }
@@ -21,5 +27,15 @@ public class Main{
             items.add(new Item(line.substring(line.indexOf("{") + 1, line.indexOf(":")), line.substring(line.indexOf(":") + 1, line.indexOf("}"))));
         }
         return items;
+    }
+
+    public static void addObject(Item item, File file) throws Exception {
+        if (file.exists() != true || file.canWrite() != true) {
+            throw new FileNotFoundException("addObject paramater file can't be written to or can't be found :(");
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file.getPath(), true));
+        writer.write("{" + item.getKey() + ":" + item.getValue() + "}");
+        writer.newLine();
+        writer.close();
     }
 }
